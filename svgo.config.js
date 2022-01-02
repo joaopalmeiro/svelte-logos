7bfe0af4380e2c2b023d04e4aa4c5684f5a017c1
João Palmeiro <joaommpalmeiro@gmail.com>
2022-01-02 00:45:53 +0000

    Closes #1

---
+++ svgo.config.js
@@ -0,0 +1,44 @@
+module.exports = {
+  multipass: true,
+
+  plugins: [
+    {
+      name: "preset-default",
+      params: {
+        overrides: {
+          removeViewBox: false,
+        },
+      },
+    },
+
+    "removeDimensions",
+
+    // https://github.com/svg/svgo/blob/main/plugins/sortAttrs.js
+    {
+      name: "sortAttrs",
+      params: {
+        xmlnsOrder: "alphabetical",
+      },
+    },
+
+    // https://github.com/svg/svgo/blob/main/plugins/removeAttrs.js
+    {
+      name: "removeAttrs",
+      params: {
+        attrs: "svg:fill:none",
+      },
+    },
+
+    // https://github.com/tailwindlabs/heroicons/blob/master/svgo.solid.yaml
+    {
+      name: "addAttributesToSVGElement",
+      params: {
+        attributes: [
+          {
+            "aria-hidden": true,
+          },
+        ],
+      },
+    },
+  ],
+};
